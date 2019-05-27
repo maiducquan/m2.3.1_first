32a4e6a75c0ca86de1a0e219ede7d1c503d60f2e
maiducquan <quanmd@magenest.com>
2019-05-27 22:17:57 +0700

    Eav Test

--- app/code/Magenest/EavTest/Setup/InstallData.php
+++ app/code/Magenest/EavTest/Setup/InstallData.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Packt\CustomerAttribute\Setup;
+
+namespace Magenest\EavTest\Setup;
+
 
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
@@ -8,30 +10,28 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 
 class InstallData implements InstallDataInterface
 {
-    private $customerSetupFactory;
-
+    private $_customerSetupFactory;
     public function __construct(
         \Magento\Customer\Setup\CustomerSetupFactory $customerSetupFactory
     ){
-        $this->customerSetupFactory = $customerSetupFactory;
+        $this->_customerSetupFactory = $customerSetupFactory;
     }
 
-    public function install(ModuleDataSetupInterface $setup,
-                            ModuleContextInterface $context)
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         /** @var CustomerSetup $customerSetup */
-        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+        $customerSetup = $this->_customerSetupFactory->create(['setup' => $setup]);
         $setup->startSetup();
-        $customerSetup->addAttribute('customer', 'loyaltynumber', [
-            'label' => 'Loyaltynumber',
+        $customerSetup->addAttribute('customer', 'avatar', [
+            'label' => 'Avatar',
             'type' => 'varchar',
-            'input' => 'text',
+            'input' => 'image',
             'required' => false,
             'visible' => true,
-            'position' => 105,
+            'position' => 5,
             'system' => false
         ]);
-        $loyaltyAttribute = $customerSetup->getEavConfig()->getAttribute('customer', 'loyaltynumber');
+        $loyaltyAttribute = $customerSetup->getEavConfig()->getAttribute('customer', 'avatar');
         $loyaltyAttribute->setData('used_in_forms', ['adminhtml_customer','customer_account_create','customer_account_edit']);
         $loyaltyAttribute->save();
         $setup->endSetup();
