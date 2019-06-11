9a5892f9fb3b152a6908f6eec41b04797f986cd8
Mai Duc Quan <quanmd@magenest.com>
2019-06-11 14:40:10 +0700

    Chapter 9

---
+++ app/code/Packt/ProductSlider/Block/Catalog/Product/ProductSlider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Packt\ProductSlider\Block\Catalog\Product;
+
+use Magento\Catalog\Block\Product\ListProduct;
+
+class ProductSlider extends ListProduct
+{
+    public function test(){
+        return 'test';
+    }
+}
